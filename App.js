e9366b7bf0a8d3c6b0b44abaa4a022979ce3405a
daheeahn <deg9810@gmail.com>
2022-11-04 18:31:55 +0900

    [part2-ch3-4-FlatList를 이용해 달력 만들기 - renderItem, ListHeaderComponent] 최종 코드

--- App.js
+++ App.js
@@ -1,13 +1,79 @@
 import dayjs from 'dayjs';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Margin from './src/Margin';
 import { runPracticeDayjs } from './src/practice-dayjs';
-import { getCalendarColumns } from './src/util';
+import { getCalendarColumns, getDayColor, getDayText } from './src/util';
+import { SimpleLineIcons } from '@expo/vector-icons'; 
+
+const columnSize = 35;
+
+const Column = ({
+  text,
+  color,
+  opacity,
+}) => {
+  return (
+    <View style={{ 
+      width: columnSize, 
+      height: columnSize, 
+      justifyContent: "center", 
+      alignItems: "center",
+      }}>
+      <Text style={{ color, opacity }}>{text}</Text>
+    </View>
+  )
+}
+const ArrowButton = ({ iconName, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={{ paddingHorizontal: 20, paddingVertical: 15 }}>
+      <SimpleLineIcons name={iconName} size={15} color="#404040" />
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   const now = dayjs();
   const columns = getCalendarColumns(now);
+
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(now).format("YYYY.MM.DD.");
+    return (
+      <View>
+        {/* < YYYY.MM.DD. > */}
+        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          <ArrowButton iconName="arrow-left" onPress={() => {}} />
+
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, color: "#404040" }}>{currentDateText}</Text>
+          </TouchableOpacity>
+
+          <ArrowButton iconName="arrow-right" onPress={() => {}} />
+        </View>
+  
+        {/* 일 ~ 토 */}
+        <View style={{ flexDirection: "row" }}>
+          {[0, 1, 2, 3, 4, 5, 6].map(day => {
+            const dayText = getDayText(day);
+            const color = getDayColor(day);
+            return (
+              <Column key={`day-${day}`} text={dayText} color={color} opacity={1} />
+            )
+          })}
+        </View>
+      </View>
+    )
+  }
+  const renderItem = ({ item: date }) => {
+    const dateText = dayjs(date).get('date');
+    const day = dayjs(date).get('day');
+    const color = getDayColor(day);
+    const isCurrentMonth = dayjs(date).isSame(now, 'month');
+    return (
+      <Column text={dateText} color={color} opacity={isCurrentMonth ? 1 : 0.4} />
+    )
+  }
 
   useEffect(() => {
     runPracticeDayjs();
@@ -16,10 +82,15 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={columns}
+        keyExtractor={(_, index) => `column-${index}`}
+        numColumns={7}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+      />
+    </SafeAreaView>
   );
 }
 
--- src/util.js
+++ src/util.js
@@ -47,3 +47,42 @@ export const getCalendarColumns = (now) => {
   // ["10.31", "11.1", ..., "11.30", "12.1", "12.2", "12.3"]
   return filledColumns;
 };
+
+/**
+ * @param day 0 ~ 6
+ * @return 일~월
+ */
+const dayTexts = ["일", "월", "화", "수", "목", "금", "토"]
+export const getDayText = (day) => {
+  /* Ex 1 */
+  return dayTexts[day];
+
+  /* Ex 2 */
+  // switch (day) {
+  //   case 0: return '일';
+  //   case 1: return '월';
+  //   case 2: return '화';
+  //   case 3: return '수';
+  //   case 4: return '목';
+  //   case 5: return '금';
+  //   case 6: return '토';
+  //   default: return '';
+  // }
+};
+
+export const getDayColor = (day) =>{
+  /* Ex 1 */
+  return day === 0 ? "#e67639" : day === 6 ? "#5872d1" : "#2b2b2b";
+  
+  /* Ex 2 */
+  // switch (day) {
+  //   case 0: return '#e67639';
+  //   case 6: return '#5872d1';
+  //   case 2:
+  //   case 3:
+  //   case 4:
+  //   case 5:
+  //   case 6:
+  //   default: return '#2b2b2b';
+  // }
+};
