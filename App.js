fc4c810ffed73ce4aaeb4442c735cc8e0da208ed
daheeahn <deg9810@gmail.com>
2022-11-04 17:35:14 +0900

    [part2-ch3-3-달력 데이터 만들기] 최종 코드

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import dayjs from 'dayjs';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { runPracticeDayjs } from './src/practice-dayjs';
+import { getCalendarColumns } from './src/util';
 
 export default function App() {
+  const now = dayjs();
+  const columns = getCalendarColumns(now);
+
   useEffect(() => {
     runPracticeDayjs();
+
+    console.log('columns', columns);
   }, []);
 
   return (
--- src/util.js
+++ src/util.js
@@ -4,11 +4,14 @@ export const fillEmptyColumns = (columns, start, end) => {
   const filledColumns = columns.slice(0);
 
   // 1. 첫날 이전 공백 채우기
-  const startDay = dayjs(start).get("day");
+  const startDay = dayjs(start).get("day"); // 화 2
   for (let i = 1; i <= startDay; i += 1) {
     const date = dayjs(start).subtract(i, "day");
     filledColumns.unshift(date);
   }
+  // ["10.31", "11.1", ..., "11.30"]
+  // ["10.30", "10.31", "11.1", ..., "11.30"]
+
   // 2. 마지막날 이후 공백 채우기
   const endDay = dayjs(end).get("day");
   /**
@@ -17,6 +20,9 @@ export const fillEmptyColumns = (columns, start, end) => {
     2 -> 4
     endDay + ? = 6
    */
+  // ["10.31", "11.1", ..., "11.30", "12.1"]
+  // ["10.31", "11.1", ..., "11.30", "12.1", "12.2"]
+  // ["10.31", "11.1", ..., "11.30", "12.1", "12.2", "12.3"]
   for (let i = 1; i <= 6 - endDay; i += 1) {
     const date = dayjs(end).add(i, "day");
     filledColumns.push(date);
@@ -25,16 +31,19 @@ export const fillEmptyColumns = (columns, start, end) => {
   return filledColumns;
 };
 export const getCalendarColumns = (now) => {
-  const start = dayjs(now).startOf("month");
-  const end = dayjs(now).endOf("month");
-  const endDate = dayjs(end).get("date");
+  const start = dayjs(now).startOf("month"); // 11.1
+  const end = dayjs(now).endOf("month"); // 11.30
+  const endDate = dayjs(end).get("date"); // 30
 
   const columns = [];
   for (let i = 0; i < endDate; i += 1) {
     const date = dayjs(start).add(i, "day");
     columns.push(date);
   }
+  console.log('columns 11월', columns);
 
   const filledColumns = fillEmptyColumns(columns, start, end);
+  console.log('columns 최종', columns);
+  // ["10.31", "11.1", ..., "11.30", "12.1", "12.2", "12.3"]
   return filledColumns;
 };
