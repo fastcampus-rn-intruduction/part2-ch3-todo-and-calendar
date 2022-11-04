4662a2255ba97b5a2a2f520736d530cf6c9e8803
daheeahn <deg9810@gmail.com>
2022-11-04 17:02:53 +0900

    [part2-ch3-2-dayjs 설치 및 사용법] 시작전코드

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { runPracticeDayjs } from './src/practice-dayjs';
 
 export default function App() {
+  useEffect(() => {
+    runPracticeDayjs();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
